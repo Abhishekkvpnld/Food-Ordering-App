130b164135cea98b5146dc36cd97d25959490092
Abhishekkv <abhishekkvpnld@gmail.com>
2024-09-30 22:26:49 +0530

    backend setup

---
+++ server/src/index.ts
@@ -0,0 +1,22 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Routes
+app.get("/", (req: Request, res: Response) => {
+  res.send("Server running...");
+});
+
+// Server
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Server started on http://localhost:${PORT}`);
+});
