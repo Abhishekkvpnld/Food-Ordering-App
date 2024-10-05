746bf771e6a850514e83ec09f2737160706bfe9e
Abhishekkv <abhishekkvpnld@gmail.com>
2024-10-05 20:01:28 +0530

    auth0 access token

--- client/src/Auth/Auth0ProviderWithNavigate.tsx
+++ client/src/Auth/Auth0ProviderWithNavigate.tsx
@@ -1,10 +1,13 @@
 import { AppState, Auth0Provider, User } from "@auth0/auth0-react";
+import { useCreateUser } from "../api/UserApi";
 
 type Props = {
   children: React.ReactNode;
 };
 
 const Auth0ProviderWithNavigate = ({ children }: Props) => {
+  const { createUser, isError, isLoading, isSuccess } = useCreateUser();
+
   const domain = import.meta.env.VITE_AUTH0_DOMAIN;
   const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
   const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
@@ -14,7 +17,9 @@ const Auth0ProviderWithNavigate = ({ children }: Props) => {
   }
 
   const onRedirectCallBack = (appState?: AppState, user?: User) => {
-    console.log("user", user);
+    if (user?.sub && user?.email) {
+      createUser({ auth0Id: user?.sub, email: user?.email });
+    }
   };
 
   return (
