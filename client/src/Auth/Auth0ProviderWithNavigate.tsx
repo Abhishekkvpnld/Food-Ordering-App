6c18b46925b6ab70baaadde969de19896733ada0
Abhishekkv <abhishekkvpnld@gmail.com>
2024-10-06 19:29:19 +0530

    validating auth0token by using jwtCheck

--- client/src/Auth/Auth0ProviderWithNavigate.tsx
+++ client/src/Auth/Auth0ProviderWithNavigate.tsx
@@ -10,10 +10,11 @@ const Auth0ProviderWithNavigate = ({ children }: Props) => {
   const domain = import.meta.env.VITE_AUTH0_DOMAIN;
   const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
   const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
+  const audience = import.meta.env.VITE_AUTH0_AUDIENCE;
 
   const navigate = useNavigate();
 
-  if (!domain || !clientId || !redirectUri) {
+  if (!domain || !clientId || !redirectUri || !audience) {
     throw new Error("Unable to initialise auth🤦");
   }
 
@@ -27,6 +28,7 @@ const Auth0ProviderWithNavigate = ({ children }: Props) => {
       clientId={clientId}
       authorizationParams={{
         redirect_uri: redirectUri,
+        audience
       }}
       onRedirectCallback={onRedirectCallBack}
     >
--- server/src/routes/userRoute.ts
+++ server/src/routes/userRoute.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import { currentUser } from "../controller/userController";
+import { jwtCheck } from "../middlewares/Auth";
 
 const router = express.Router();
 
 // api/user/
-router.post("/create-user",currentUser);
+router.post("/create-user",jwtCheck,currentUser);
 
 export default router;
