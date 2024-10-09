968b28d72ed9039ff457dc55db46aad6dde8c65a
Abhishekkv <abhishekkvpnld@gmail.com>
2024-10-09 14:06:04 +0530

    cloudinary setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,11 +4,18 @@ import dotenv from "dotenv";
 import morgan from "morgan";
 import dbConnection from "./config/database";
 import userRoute from "./routes/userRoute";
-
+import { v2 as cloudinary } from "cloudinary";
 
 dotenv.config();
 
 const app = express();
+
+// cloudinary setup
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 // Middleware
 app.use(express.json());
@@ -16,13 +23,11 @@ app.use(cors());
 app.use(morgan("dev"));
 
 // Routes
-app.use("/api/user",userRoute);
-
+app.use("/api/user", userRoute);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Server running...🚀🚀");
 });
-
 
 // Server
 const PORT = process.env.PORT || 4000;
